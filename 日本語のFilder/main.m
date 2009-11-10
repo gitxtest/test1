603925fcb6f7f1141d7edfb75db5285b80bf67cb
nettani <nettani@MBPro17inch.local>
2009-11-10 15:36:48 +0900

    日本語のFolderだよ!

---
+++ 日本語のFilder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iPh1
+//
+//  Created by nettani on 08/07/12.
+//  Copyright A10 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
